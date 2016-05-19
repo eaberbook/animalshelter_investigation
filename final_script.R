ef39b0e056410eedbe0d1adb289fa3861ab1858f
eaberbook <ericaberbook@ucla.edu>
2016-05-19 14:26:42 -0700

    Update final_script.R

--- final_script.R
+++ final_script.R
@@ -36,10 +36,10 @@ test <- animals[-trainindex,]
 # MULTINOMIAL MODEL
 
 library(nnet)
-multinomial_model <- multinom(OutCatg~Days.In.Shelter+Intake.Age+Sex+Intake.Type+Species+Microchip.Status
-                              +Shelter,data=train)
+multinomial_model <- multinom(OutCatg~Primary.Breed+Days.In.Shelter+Intake.Age+Sex+Intake.Type+Species+Microchip.Status
+                              +Shelter+License.Status+Month,data=train)
 
-multinomial_preds <- predict(multinomial_model,newdata=test,type="class")
+multinomial_preds <- predict(multinomial_model,newdata=real_test,type="class")
 
 final_multinomial_probs <- predict(multinomial_model,newdata=real_test,type="prob")
 final_frame<-cbind(ARN,final_multinomial_probs)
@@ -50,18 +50,23 @@ table(multinomial_preds,test$OutCatg)
 
 # RANDOM FOREST MODEL
 library(randomForest)
-improved_rf <- randomForest(OutCatg~Days.In.Shelter+Intake.Age+Sex+Intake.Type+Species+Microchip.Status+Shelter+Year,data=train,type="classification",importance=TRUE,na.action=na.exclude)
+improved_rf <- randomForest(OutCatg~Days.In.Shelter+Intake.Age+Sex+Intake.Type+Species+Microchip.Status+Shelter+Year+License.Status,data=animals,type="classification")
+
+# Calculation of variable importance, trying the varImp function.
+varImp(improved_rf)
+
+
 print(improved_rf)
-improved_rf_preds <- predict(improved_rf,newdata=test,type="response")
+improved_rf_preds <- predict(improved_rf,newdata=test,type="prob")
 rf_table<-table(improved_rf_preds,test$OutCatg)
 class_rate(rf_table)
-
 
 # This is how I bound random forest for export
 final_rf_probs <- predict(improved_rf,newdata=real_test,type="prob")
 final_rf_probs <- as.data.frame(final_rf_probs)
 ARN<-real_test$ARN
-final_frame<-cbind(ARN,final_rf_probs)
+final_frame<-rbind(ARN,final_rf_probs)
+final_frame<-rbind(real_test$ARN,improved_rf_preds)
 row.names(final_frame)<-NULL
 write.csv(final_frame,file="finalpredictions")
 
@@ -69,17 +74,33 @@ write.csv(final_frame,file="finalpredictions")
 library(caret)
 
 
-av_nnet <- avNNet(OutCatg~Days.In.Shelter+Intake.Age+Sex+Intake.Type+Species+Microchip.Status+Shelter+Year,data=animals,type="classification",size=10)
-av_nnet_predictions <- predict(av_nnet,real_test,type="class")
+av_nnet <- avNNet(OutCatg~Days.In.Shelter+Intake.Age+Sex+Intake.Type+Species+Microchip.Status+Shelter+Year+License.Status,data=animals,type="classification",size=12)
+av_nnet_predictions <- predict(av_nnet,real_test)
 
-# Neural network here is giving me an 80% prediction.
+# Neural network here is giving me an 79.5% prediction.
 
 av_nnet_predictions2 <- predict(av_nnet,test,type="class")
 table(av_nnet_predictions2,test$OutCatg)
 
 
+# Trying BagFDA
+library(mda)
+library(earth)
+bag_fda_model <- bagFDA(OutCatg~Days.In.Shelter+Intake.Age+Sex+Intake.Type+Species+Microchip.Status+Shelter+Year+License.Status,data=animals)
+  
+special_data <- model.matrix(~Days.In.Shelter+Intake.Age+Sex+Intake.Type+Species+Microchip.Status+Shelter+Year+License.Status,data=real_test)
+
+bag_fda_preds<-predict(bag_fda_model,newdata=special_data,type="probs")
+
+
+
+ARN <-real_test$ARN
+
+final_frame <- cbind(ARN,bag_fda_preds)
+
+
 # Binding av_nnet for export
-final_frame <- cbind(real_test$ARN,av_nnet_predictions)
+final_frame <- cbind(ARN,av_nnet_predictions)
 write.csv(final_frame,file="finalpredictions.csv")
 
 # Knn
@@ -105,6 +126,7 @@ class_rate<-function(table){
 # CLEANING DATA FUNCTION
 clean_data<-function(animals){
   
+  animals$ARN <- as.factor(animals$ARN)
   animals$DOB <- as.Date(animals$DOB)
   animals$S.N.Date <- as.Date(animals$S.N.Date)
   animals$Intake.Date <- as.Date(animals$Intake.Date)
@@ -140,13 +162,22 @@ clean_data<-function(animals){
   animals$License.Status[is.na(animals$License.Status)]<-"Yes"
   animals$License.Status <- as.factor(animals$License.Status)
   
+  animals$SN.Status[which(is.na(animals$S.N.Date)==T)]<-"No"
+  animals$SN.Status[is.na(animals$SN.Status)]<-"Yes"
+  animals$SN.Status <- as.factor(animals$SN.Status)
+  
   # S.N. status to Yes or No
   
   
   # Creating a year variable to see if significant
   animals$Year <- format(animals$Outcome.Date,'%Y')
   animals$Year <- as.factor(animals$Year)
-    
+  
+  #Creating a month variable to see if significant
+  animals$Month <- format(animals$Outcome.Date,'%m')
+  animals$Month <- as.numeric(animals$Month)
+
+  
   # Turning NA's to 0's in some of the columns.
   # Days.In.Shelter. Replace NA's with 0's.
   animals$Days.In.Shelter[is.na(animals$Days.In.Shelter)]<-0
@@ -154,6 +185,8 @@ clean_data<-function(animals){
   animals$Intake.Age[is.na(animals$Intake.Age)]<-mean(animals$Intake.Age,na.rm=TRUE)
   # Year
   animals$Year[is.na(animals$Year)]<-2011
+  animals$Month[is.na(animals$Month)]<-7
+  animals$Month <- as.factor(animals$Month)
   return(animals)
 }
 
